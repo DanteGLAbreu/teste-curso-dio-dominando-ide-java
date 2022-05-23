a38478fd78011a90a11afeae87dc0550208b7cc8
DanteGLAbreu <danteglabreu@gmail.com>
2022-05-23 15:16:45 -0300

    criando class livro

--- IntelliJ/primeiro-projeto-intellij/src/br/com/dio/PrimeiroPrograma.java
+++ IntelliJ/primeiro-projeto-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -8,8 +8,44 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro1 = new Livro("O Último Olimpiano",381);
+        System.out.println(livro1);
+
         /*int a = 5;
         int b = 3;
         System.out.println("Hello World " + (a+b));*/
+    }
+}
+class Livro {
+    private String name;
+    private Integer pageNumb;
+
+    public Livro(String name, Integer pageNumb) {
+        this.name = name;
+        this.pageNumb = pageNumb;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPageNumb() {
+        return pageNumb;
+    }
+
+    public void setPageNumb(Integer pageNumb) {
+        this.pageNumb = pageNumb;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "name='" + name + '\'' +
+                ", pageNumb=" + pageNumb +
+                '}';
     }
 }
